bbfc183c396dc14d8df37ab31b63bb0d0e94b5af
ragav7332 <96164910+ragav7332@users.noreply.github.com>
2023-12-09 12:20:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+/** @format */
+
+import express from "express";
+import AppRouter from "./routers/router.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+const PORT = Number(process.env.PORT);
+
+app.use(express.json());
+app.use("/", AppRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Server! 🌐");
+});
+
+mongoose
+  .connect(`${process.env.DB_URL}`)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
